7cc0787fd637b9dea0222f2973ed4d0573a60040
mahdirahmani1376 <rahmanimahdi16@gmail.com>
2022-12-03 18:29:09 +0330

    3

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +20,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin', [App\Http\Controllers\AdminsController::class, 'index'])->name('admin.index');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/admin', [AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post',[PostController::class,'show'])->name('post');
